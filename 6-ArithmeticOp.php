aa93f6ac3d0ff8bde0ed61839c54653892b66577
Philip Marfo <philcho98@gmail.com>
2021-03-21 13:36:31 +0000

    Finding modulo

--- 6-ArithmeticOp.php
+++ 6-ArithmeticOp.php
@@ -1,10 +1,12 @@
 <?php
-$num1 = 1;
-$num2 = 4;
+$num1 = 8;
+$num2 = 2;
 echo $total = $num1 + $num2. '<br>';
 echo $total = $num1 - $num2. '<br>';
 echo $total = $num1 / $num2. '<br>';
 echo $total = $num1 * $num2. '<br>';
+echo $total = $num1 % $num2. '<br>';
+
 
 
 
