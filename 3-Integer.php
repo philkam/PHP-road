50541ec08ef551872fe82cbfe93988c9cec6cd11
Philip Marfo <philcho98@gmail.com>
2021-03-21 12:29:47 +0000

    Integers in PHP

---
+++ 3-Integer.php
@@ -0,0 +1,14 @@
+<?php
+
+echo 5-3 ,'<br>';
+echo 29+23, '<br>';
+echo 3*2 ,'<br>';
+echo 10/2 ,'<br>';
+
+//vardump gives the datatype
+echo 4.8 + 5.27 ,'<br>';
+echo var_dump(3.8) ,'<br>' ;
+echo var_dump('Hello'), '<br>';
+echo var_dump(9);
+
+?>
