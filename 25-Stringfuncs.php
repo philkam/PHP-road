6f8ce83f2788f70e6c688bd9640747917da77d39
Philip Marfo <philcho98@gmail.com>
2021-03-21 20:34:16 +0000

    String function for substring and stringposition

---
+++ 25-Stringfuncs.php
@@ -0,0 +1,12 @@
+<?php
+ //String functions: substr() & strpos();
+
+    $str = "Hello my Students! I&apos;m doing my work here in the room";
+    echo substr($str, 0,5); //substr(string, begin, end) extracts hello from string
+    echo "<br>";
+    echo strpos($str,"s"); // string position (string, the character's position)
+    echo strpos($str,"my",8) //string postion(string, "Substring", after the 8th)
+
+
+
+?>
