2c4d92f4c9ba2b1139a95b9f40bffc4e4617696b
Philip Marfo <philcho98@gmail.com>
2021-03-21 12:48:12 +0000

    The use of '.' as concatenation in PHP

---
+++ 5-Concatenation.php
@@ -0,0 +1,14 @@
+<?php
+// #Concatenation is bringing things together
+// #The "." represents concatenation
+
+$var1 = 2;
+$var2 = 43;
+$var3 = 534;
+$var4 = $var1 + $var2 + $var3;
+
+
+echo $var4.'<br>';
+echo $var4.' This is concatenated'.'<br>';
+echo $var1.$var2;
+?> 
