b746b80f88f00181e194e49ce683474c7b3491b6
Philip Marfo <philcho98@gmail.com>
2021-03-21 20:05:52 +0000

    local variable because  is inside the function and can be accessed

---
+++ 23-GlobalScope.php
@@ -0,0 +1,18 @@
+<?php
+ 
+
+
+function func(){
+      $val1 ='Cat'; //local variable because it is inside the function */
+
+
+
+    echo "This is a $val1";
+}
+
+
+//function call
+func();
+ 
+
+?>
