bf7b7558adc8c51f56546926c308a4022e4b6afe
Philip Marfo <philcho98@gmail.com>
2021-03-21 19:05:16 +0000

    Using the switch-statement in PHP

---
+++ 15-SwitchStatement.php
@@ -0,0 +1,22 @@
+<?php
+$fruitName = 'Appl';
+
+switch($fruitName){
+    case 'Apple':
+        echo 'This is a red apple';
+    break;
+    case 'Mango':
+        echo 'This is a yellow mango';
+    break;
+    case 'Orange':
+        echo 'This is an orange';
+    break;
+    case 'Banana':
+        echo 'This is a banana';
+    break;
+    default:
+    echo 'The fruit you want is not available at the mall now';
+break;
+ 
+}
+?>
