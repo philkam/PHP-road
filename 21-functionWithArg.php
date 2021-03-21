ba32dbc1a811ed2caca3f9657deb72f8ead4b4ef
Philip Marfo <philcho98@gmail.com>
2021-03-21 19:51:12 +0000

    Functions with Arguments in PHP

---
+++ 21-functionWithArg.php
@@ -0,0 +1,17 @@
+<?php
+
+function func1($val1,$val2){
+    echo "$val1 I just want to say $val2 <br>";
+}
+
+func1("Keren","Hello", );
+func1("Mandy","How are you?");
+func1("Samantha", "I am Fine ");
+
+function calc($val3,$val4){
+    echo $val3 + $val4;
+}
+
+calc(3,5);
+
+?>
