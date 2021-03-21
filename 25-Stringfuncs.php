b803eac9a341099554d8f8736b57d32e2e33332d
Philip Marfo <philcho98@gmail.com>
2021-03-21 20:47:02 +0000

    trim() removes spaces before and and after a string

--- 25-Stringfuncs.php
+++ 25-Stringfuncs.php
@@ -1,11 +1,16 @@
 <?php
- //String functions: substr() & strpos();
+ //String functions: substr() & strpos() ;
 
     $str = "Hello my Students! I&apos;m doing my work here in the room";
     echo substr($str, 0,5); //substr(string, begin, end) extracts hello from string
     echo "<br>";
     echo strpos($str,"s"); // string position (string, the character's position);
-    echo strpos($str,"my",8) //string postion(string, "Substring", after the 8th);
+    echo strpos($str,"my",8) ;//string postion(string, "Substring", after the 8th);
+    echo "<br>";
+
+    
+    $str1 = ' This is a string ';
+    echo trim($str1).'Google';
 
 
 
