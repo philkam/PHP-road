a61dc585d2926576fb1ceee09129e6b325e9f810
Philip Marfo <philcho98@gmail.com>
2021-03-21 12:57:19 +0000

    Using arithmetic operations

---
+++ 6-ArithmeticOp.php
@@ -0,0 +1,12 @@
+<?php
+$num1 = 1;
+$num2 = 4;
+echo $total = $num1 + $num2. '<br>';
+echo $total = $num1 - $num2. '<br>';
+echo $total = $num1 / $num2. '<br>';
+echo $total = $num1 * $num2. '<br>';
+
+
+
+
+?>
