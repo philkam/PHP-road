a8f0c2fb04688962a152f6684369cc767b951300
Philip Marfo <philcho98@gmail.com>
2021-03-21 14:20:54 +0000

    Understanding assignment operators

---
+++ 7-AssignmentOp.php
@@ -0,0 +1,17 @@
+<?php
+//Assignment Operator  =
+
+$var1 = 7;
+$var2 = 8;
+$var5 = 3;
+
+echo $ans = $var1 + $var2;
+echo '<br>'.$ans += $var5;
+echo '<br>'.$ans -= $var5;
+echo '<br>'.$ans *= $var5;
+echo '<br>'.$ans /= $var5;
+echo '<br>'.$ans /= $var5;
+
+
+
+?>
