f18343fdc00c5af4e06d7a1073248c5fe8f16b52
Philip Marfo <philcho98@gmail.com>
2021-03-21 14:35:30 +0000

    Increment and decrement Operators

---
+++ 8-IncrementnDecrementOp.php
@@ -0,0 +1,16 @@
+<?php
+//Increment and Decrement Operator
+
+$var1 = 2;
+$var2 = 3;
+
+//First way-Add before variable
+echo ++$var1;
+
+//Second way-add after teh variable and output
+ $var1++;
+echo $var1.'<br>';
+
+echo --$var2;
+
+?>
