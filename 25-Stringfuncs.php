be65cb18bc093b3c131d3fdc368184560c55f34b
Philip Marfo <philcho98@gmail.com>
2021-03-21 20:51:08 +0000

    ucfirst capitalises the first alphabet

--- 25-Stringfuncs.php
+++ 25-Stringfuncs.php
@@ -1,7 +1,7 @@
 <?php
  //String functions: substr() & strpos() ;
 
-    $str = "Hello my Students! I&apos;m doing my work here in the room";
+    $str = "hello my Students! I&apos;m doing my work here in the room";
     echo substr($str, 0,5); //substr(string, begin, end) extracts hello from string
     echo "<br>";
     echo strpos($str,"s"); // string position (string, the character's position);
@@ -9,12 +9,17 @@
     echo "<br>";
 
 
-    $str1 = ' This is a string ';
+    $str1 = ' this is a string ';
     echo trim($str1).'Google';
     //echo $str.'Google' // adds spaces
     echo "<br>";
 
     echo strlen($str1); 
+    echo "<br>";
+
+    echo ucfirst($str)
+
+
 
 
 
