76a13f6c42634c275e3b58f1743f11e5106220c5
Philip Marfo <philcho98@gmail.com>
2021-03-22 14:46:57 +0000

    More on Arrays

---
+++ 29-ArrayIdentifier.php
@@ -0,0 +1,18 @@
+<?php
+
+//PHP automatically indexes array.
+$countries = array();
+$countries[] = 'Ghana';
+$countries[] = 'Kenya';
+$countries[] = 'Nigeria';
+$countries[] = 'Rwanda';
+$countries[] = 'South Africa';
+
+echo $countries[2].'<br>'; 
+
+$continents = array('Africa','Asia','Antarctica','America', 'Australia');
+$continents[] = 'Europe';
+echo $continents[5];
+
+
+?>
