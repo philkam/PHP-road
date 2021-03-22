4d61f113a7ba33abc5a65e3c03de2e926837680f
Philip Marfo <philcho98@gmail.com>
2021-03-22 14:34:00 +0000

    Arrays in PHP

---
+++ 28-ArrayConstruct.php
@@ -0,0 +1,18 @@
+<?php
+$days = array();
+$days[0] ='Monday';
+$days[1] = 'Tuesday';
+$days[2] = 'Wednesday';
+$days[3] = 'Thurday';
+$days[4] =  'Friday';
+$days[5] = 'Saturday';
+$days[6] = 'Sunday';
+
+echo $days[4].'<br>';
+
+$months = array('January', 'February', 'March', 'April', 'May', 'June', 'July');
+echo $months[2];
+
+
+
+?>
