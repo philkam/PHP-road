1fc1868bbba13ff21767da5444cf6459f59f972b
Philip Marfo <philcho98@gmail.com>
2021-03-23 10:59:14 +0000

    "Printing out using for each loop"

--- 29-ArrayIdentifier.php
+++ 29-ArrayIdentifier.php
@@ -12,7 +12,11 @@ echo $countries[2].'<br>';
 
 $continents = array('Africa','Asia','Antarctica','America', 'Australia');
 $continents[] = 'Europe';
-echo $continents[5];
+echo $continents[5].'<br><br><br>';
 
 
+//Using foreach to print out an array
+foreach($countries as $c){
+    echo $c.'<br>';
+}
 ?>
