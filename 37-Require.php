cc9fe4b1d6275d2f61817501a3744d20488cd2d1
Philip Marfo <philcho98@gmail.com>
2021-03-23 13:48:32 +0000

    Shows how require and include works in PHP

---
+++ 37-Require.php
@@ -0,0 +1,11 @@
+<?php
+
+//for include in PHP if the file does not exist the code will run after the include command
+//for require in PHP if the file does not exist the code will not run. expresses necessity
+
+require '36-forIncludeAndRequire.php';
+//include '36-forIncludeAndRequire.php';
+echo "We are talking about the $var1"
+
+
+?>
