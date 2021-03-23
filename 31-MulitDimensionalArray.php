27053bb5fc3a2791ee0fe5fb4859f9e6903ef378
Philip Marfo <philcho98@gmail.com>
2021-03-23 11:14:12 +0000

    "Printing MulitDimensionalArray using foreach"

--- 31-MulitDimensionalArray.php
+++ 31-MulitDimensionalArray.php
@@ -6,9 +6,16 @@ $employees = array('employee1' => array(
 ));
 
 echo $employees['employee1']['name'].'<br>';
-echo $employees['employee2']['skill'];
+echo $employees['employee2']['skill'].'<br><br><br>';
+
+//Printing arrays with different looping techniques
+foreach($employees as $emp){
+    foreach($emp as $key => $data){
+        echo $key. '=> '. $data.'<br>';
+    }
 
 
+}
 
 
 
