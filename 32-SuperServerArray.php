99b9089c5281b37015a4da4b53823cd04b76687b
Philip Marfo <philcho98@gmail.com>
2021-03-23 12:44:43 +0000

    Server Super global array in PHP

---
+++ 32-SuperServerArray.php
@@ -0,0 +1,12 @@
+<?php
+echo $_SERVER['PHP_SELF']; //address of webpage
+echo '<br><br>';
+echo $_SERVER['HTTP_USER_AGENT']; //browser using
+echo '<br><br>';
+echo $_SERVER['REMOTE_ADDR']; //IP address
+echo '<br><br>';
+echo $_SERVER['HTTP_HOST']; //server
+echo '<br><br>';
+echo $_SERVER['REQUEST_URI']; //url
+
+?>
