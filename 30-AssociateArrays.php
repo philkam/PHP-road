e40f98a88ba126b500ba89fbf2050d9877cb8ecf
Philip Marfo <philcho98@gmail.com>
2021-03-23 11:08:41 +0000

    Printing Associative array with foreach loop

--- 30-AssociateArrays.php
+++ 30-AssociateArrays.php
@@ -12,6 +12,17 @@ $days['mon'] = 'Monday';
 $days['tue'] = 'Tuesday';
 $days[1] = 'Wednesday';
 
-echo $days[1];
+echo $days[1].'<br><br><br>';
+
+//using foreach to print associativeArray
+foreach($tasks as $key => $task){
+    echo $key.'<br>'; //prints key
+    
+    echo $task.'<br>'; //print task
+
+    echo $key.' => '.$task.'<br>'; //prints key with task
+}
+
+
 
 ?>
