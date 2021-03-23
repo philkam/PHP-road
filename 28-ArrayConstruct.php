3e2457d700d7b85ee9d29f6c2b8d9e2f59e6f804
Philip Marfo <philcho98@gmail.com>
2021-03-23 11:31:59 +0000

    Array functions

--- 28-ArrayConstruct.php
+++ 28-ArrayConstruct.php
@@ -17,8 +17,31 @@ echo $months[2].'<br><br><br>';
 for($c = 0; $c <=6; $c++){
    echo  $days[$c].'<br>';
 }
-
+echo '<br><br><br>';
 //print_r to print rawcode
 print_r($days);
+echo '<br><br><br>';
+
+//count items in array
+echo count($days);
+echo '<br><br><br>';
+
+//check if string is in array
+echo in_array('Monday',$days).'<br>'; //Gives true(1) or false(1)
+
+$s_day = 'Tuesday';
+if (in_array($s_day,$days)){
+    echo "$s_day is in the array<br><br>";
+}else{
+    echo "It is not there<br><br>";
+}
+
+//Check if it is an array
+if(is_array($days)){
+    echo "it is an array";
+}else{
+    echo "It is not an array";
+}
+
 
 ?>
