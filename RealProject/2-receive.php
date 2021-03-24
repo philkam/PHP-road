bce1c49ad8a70f101c67f4f41d479ad15d1e6482
Philip Marfo <philcho98@gmail.com>
2021-03-24 02:47:16 +0000

    Add information about the difference between get and push and use isset with condition to limit getting details from page even after refresh

--- RealProject/2-receive.php
+++ RealProject/2-receive.php
@@ -1,7 +1,18 @@
 <?php
+    /*Get sends through data through URL but post doesn't, post is secure.
+    echo "The username is : $_GET[login_email] <br>";
+    echo "The password is : $_GET[login_password] <br>";
+    */
 
-echo "The username is : $_GET[login_email] <br>";
-echo "The username is : $_GET[login_password] <br>";
+if(isset($_POST['validate'])){
+    echo "The username is : $_POST[login_email] <br>";
+    echo "The pasword is : $_POST[login_password] <br>";
+
+}else{
+    echo"Sorry, There&apos;s no request received";
+}
+
+
 
 
 
