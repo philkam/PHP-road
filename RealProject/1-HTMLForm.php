e003a6cfe64cedd340ffacfa554d524bc2fcca90
Philip Marfo <philcho98@gmail.com>
2021-03-24 02:39:32 +0000

    Learning PHP and HTML

---
+++ RealProject/1-HTMLForm.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<title>PHP Form</title>
+</head>
+<body>
+    <form action="2-receive.php">
+        <table>
+            <tr>
+                <td>E-mail</td>
+                <td><input type="email" name="login_email"></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td><input type="password" name ="login_password"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="Submit" name= "login_submit"></td>
+            </tr>
+        </table>
+
+
+</form>
+
+
+</body>
+
+
+</html>
---
+++ RealProject/2-receive.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "The username is : $_GET[login_email] <br>";
+echo "The username is : $_GET[login_password] <br>";
+
+
+
+?>
