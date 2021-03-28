418711de9e6c4624971cf08d4462ac079cd75e65
Philip Marfo <philcho98@gmail.com>
2021-03-28 23:38:42 +0000

    Added the db file in includes

---
+++ RealProject/includes/db.php
@@ -0,0 +1,14 @@
+<?php 
+
+$server = 'localhost';
+$username = 'root';
+$password = '';
+$db = 'cms';
+
+$conn = mysqli_connect($server,$username,$password,$db);
+
+if(!$conn){
+    die("Connection failed".mysqli_connect_error());
+}
+
+?>
