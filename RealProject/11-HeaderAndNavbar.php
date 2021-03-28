9fe579a243e095bf97df9bc6fac1e85338051c7f
Philip Marfo <philcho98@gmail.com>
2021-03-28 01:20:19 +0000

    Navbar created using the bootstrap

---
+++ RealProject/11-HeaderAndNavbar.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+        <title>Bootstrap Navbar and Header</title>
+      <link rel="stylesheet" type = "text/css" href ="css/bootstrap.css">
+      <script type="text/javascript" src="js/jquery.min.js"></script>
+     <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+    </head>
+    <body>
+        <header class="navbar navbar-inverse navbar-static-top">
+            <div class="container">
+                <a href="index.php" class="navbar-brand">Phil System</a>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="active"><a href="#">Home</a>
+                    <li><a href="#">Article</a>
+                    <li><a href="#">Contact</a>
+                    <li><a href="#">Log out</a>
+                </ul>
+            </div>
+        </header>
+        <div class="container">
+            <article class="row">
+                <section></section>
+                <aside></aside>
+            </article>
+        </div>
+
+    </body>
+</html>
