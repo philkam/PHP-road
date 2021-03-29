5debef49de1c037d3d9e561392cc200d3870115d
Philip Marfo <philcho98@gmail.com>
2021-03-29 13:53:37 +0000

    Included an active clas in the asidebar on the post page

--- RealProject/includes/aside.php
+++ RealProject/includes/aside.php
@@ -47,10 +47,28 @@
                     $sel_side = "SELECT * FROM posts";
                     $run_side = mysqli_query($conn,$sel_side);
                     while($rows = mysqli_fetch_assoc($run_side)){
+                        if(isset($_GET['post_id'])){
+                            if($_GET['post_id'] == $rows['id']){
+                                $class = 'active';
+
+                            }else{
+                                $class ='';
+                            }
+                        }else{
+                            $class= '';
+                        }
                         echo '
-                        <a href="12-Postpage.php?post_id='.$rows['id'].'" class="list-group-item">
+                        <a href="12-Postpage.php?post_id='.$rows['id'].'" class="list-group-item '.$class.'">
+                            <div class="col-sm-4">
+                                <img src="'.$rows['image'].'" width=100%>
+                            </div>
+                            <div class="col-sm-8">
                             <h4 class="list-group-item-heading">'.$rows['title'].'</h4>
-                            <p class="list-group-item-text">'.substr($rows['description'],0,100).' </a>
+                            <p class="list-group-item-text">'.substr($rows['description'],0,100).'</p>
+                            </div>
+                            <div style="clear:both;"></div>
+                            
+                            </a>
                         ';
 
                     }
