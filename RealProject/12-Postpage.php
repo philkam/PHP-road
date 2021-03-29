5772995890c345c30b72e402cb984899caf44099
Philip Marfo <philcho98@gmail.com>
2021-03-29 03:40:35 +0000

    Header file

--- RealProject/12-Postpage.php
+++ RealProject/12-Postpage.php
@@ -11,35 +11,8 @@
 
     </head>
     <body>
-        <header class="navbar navbar-inverse navbar-static-top ">
-            <div class="container">
-                <a href="11-index.php" class="navbar-brand">Phil System</a>
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="active"><a href="#">Home</a>
-                    <?php
-                    $sel_cat = "SELECT * FROM category";
-                    $run_cat  = mysqli_query($conn,$sel_cat);
-                    while($rows = mysqli_fetch_assoc($run_cat)){
-                        if(isset($_GET['cat_name'])){
-                            if($_GET['cat_name']==$rows['category_name']){
-                                $class = 'active';
-                            }else{
-                                $class = '';
-                            }
-                        }else{
-                            $class = '';
+    <?php include 'includes/header.php'?>
 
-                        }
-                        echo '<li class="'.$class.'"><a href="13-Menupage.php?cat_name='.$rows['category_name'].'">'.$rows['category_name'].'</a></li>';
-                  
-                    }
-                    ?>
-
-                    <li><a href="#">Contact</a>
-                    <li><a href="#">Log out</a>
-                </ul>
-            </div>
-        </header>
         <div class="container">
             <article class="row">
                 <section class ="col-lg-8">
